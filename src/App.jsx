d282276443227ff5498bdbd2d44c21e53d73455c
Edgar Méndez <mdzredgar@gmail.com>
2023-01-19 14:18:39 -0600

    Limpieza de los archivos

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="content">
+
+        </div>
+        <div className="author">
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
